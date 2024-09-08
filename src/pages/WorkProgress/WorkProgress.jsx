c87ec7e2b17900b788be67f91b10df63dfaae138
Ankan Mandal <72788172+ankan-mandal-1@users.noreply.github.com>
2024-09-08 22:06:47 +0530

    Add files via upload

---
+++ src/pages/WorkProgress/WorkProgress.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WorkProgress = () => {
+  return (
+    <div className="dashboard box_over">
+        <div className="center_div">
+            <h1 className="heading">Work in Progress!</h1>
+            <p>🚧 This Page is under Construction!</p>
+        </div>
+    </div>
+  )
+}
+
+export default WorkProgress
